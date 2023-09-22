62849272a8637876a251da83d337cb212f918489
Stéphane Nsakala <stephane.nsakala@hopitauxschuman.lu>
2023-09-22 10:29:46 +0200

    use fsutil to change image size to 1MB

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,6 @@
 cargo bootimage
 fsutil file seteof .\target\x86_64-minios\debug\bootimage-minios.bin 1048576
-del "C:\Users\stephane.nsakala\VirtualBox VMs\minios.vdi"
+del "C:\Users\stephane.nsakala\VirtualBox VMs\minios\minios.vdi"
 VBoxManage convertfromraw .\target\x86_64-minios\debug\bootimage-minios.bin "C:\Users\stephane.nsakala\VirtualBox VMs\minios\minios.vdi" --variant Fixed
 vboxmanage internalcommands sethduuid "C:\Users\stephane.nsakala\VirtualBox VMs\minios\minios.vdi" f90ea889-242f-4edd-aba7-55850076dca6
 VBoxManage.exe startvm minios
